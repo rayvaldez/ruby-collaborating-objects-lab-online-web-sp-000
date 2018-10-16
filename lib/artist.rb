7c48a756031e2c8c22caee36c52c8854fed083b4
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-16 21:11:34 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,30 @@
+class Artist
+  attr_accessor :name, :songs
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,16 @@
+require 'pry'
+class Song
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(filename)
+    song_name = filename.split(" - ")[1]
+    new_song = self.new(song_name)
+
+    new_song.artist_name = filename.split(" - ")[0]
+    new_song
+  end
+end
